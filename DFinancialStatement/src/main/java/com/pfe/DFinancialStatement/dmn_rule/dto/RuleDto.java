8b041bab47bea6c282e9421196be6b76732eb1c5
mahdiJ2001 <mahdio.jellali@hotmail.fr>
2025-05-17 19:52:31 +0200

    DmnXml transformation service

---
+++ DFinancialStatement/src/main/java/com/pfe/DFinancialStatement/dmn_rule/dto/RuleDto.java
@@ -0,0 +1,15 @@
+package com.pfe.DFinancialStatement.dmn_rule.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RuleDto {
+    private String expression;
+    private String condition;
+    private String messageErreur;
+    private String severite;
+}
